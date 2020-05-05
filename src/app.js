89d2a372a8a1fbe26cd964ee9d71b5ce856295dc
willconstable <will_constable@msn.com>
2020-05-05 13:16:40 +0100

    first commit, add basic layout and portfolio section layout

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import "./scss/main.scss";
+
+//Grab scroll position
+let prevScrollPos = window.pageYOffset;
+
+// Function to controll navbar during scroll
+window.onscroll = function() {
+    //Grab current scroll position
+    let currentScrollPos = window.pageYOffset;
+    //Check position of scroll and displah navbar accordingly
+    if(prevScrollPos > currentScrollPos) {
+        document.getElementById("nav").style.top = "0";
+        document.getElementById("nav").style.opacity = "1";
+    } else {
+        document.getElementById("nav").style.top = "-50px";
+        document.getElementById("nav").style.opacity = "0";
+    }
+    prevScrollPos = currentScrollPos;
+}
